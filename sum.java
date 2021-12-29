f8df5bfac83bda52b659774322d79ebc246a3a1c
Naveen Baliga <navba@microsoft.com>
2021-12-29 15:03:23 +0530

    initial commit with all files

--- sum.java
+++ sum.java
@@ -3,7 +3,7 @@ public class sum {
     public static void main(String[] args){
         int a = 5;
         int b = 10;
-        System.out.println(a+b);
+        System.out.println("sum is" + a+b);
     }
 
 }
